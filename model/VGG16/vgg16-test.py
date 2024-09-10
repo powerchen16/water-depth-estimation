bf5cd3573ce297ae028481b298b4a344bcbf6e50
powerchen16 <155006365+powerchen16@users.noreply.github.com>
2024-09-11 00:07:33 +0800

    Update vgg16-test.py

--- model/VGG16/vgg16-test.py
+++ model/VGG16/vgg16-test.py
@@ -25,7 +25,7 @@ model = model.to(device)
 model.load_state_dict(torch.load(path))      # 再加载模型参数
 
 # 读取测试集中的数据
-root_test = r'D:\qianmengchen\water depth prediction\dataset-1\test'
+root_test = r'D:\water depth prediction\dataset-1\test'
 test_dataset = Waterlevel(root_test)
 test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 with torch.no_grad():
